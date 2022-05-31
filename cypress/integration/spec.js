f23e86c9b2a23b5720b109e972841780c7e1ece7
Matt Schile <mschile@cypress.io>
2022-05-31 14:22:30 -0600

    replicating selectFile issue

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,30 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('/cypress/fixtures/index.html')
+
+    cy.fixture('example.json', { encoding: null }).as('file')
+    cy.get('#file').selectFile('@file')
+
+    cy.get('#file')
+      .then((input) => {
+        expect(input[0].files.length).to.eq(1)
+        expect(input[0].files[0].name).to.eq('example.json')
+        expect(input[0].files[0].type).to.eq('application/json')
+      })
+  })
+
+  it('works on subsequent test', () => {
+    cy.visit('/cypress/fixtures/index.html')
+
+    cy.fixture('example.json', { encoding: null }).as('file')
+    cy.get('#file').selectFile('@file')
+
+    cy.get('#file')
+      .then((input) => {
+        expect(input[0].files.length).to.eq(1)
+        expect(input[0].files[0].name).to.eq('example.json')
+        expect(input[0].files[0].type).to.eq('application/json')
+      })
   })
 })
